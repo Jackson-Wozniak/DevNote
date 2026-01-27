310eecfb6ad3b4f9dcb7d8d7b7620a61b17a997b
Jackson Wozniak <jjw30@comcast.net>
2026-01-26 21:34:40 -0500

    add more optional fields to entry

--- src/DevBank/Commands/SaveCommand.cs
+++ src/DevBank/Commands/SaveCommand.cs
@@ -37,8 +37,20 @@ public class SaveCommand
         {
             AllowMultipleArgumentsPerToken = true
         };
+        var projectsOption = new Option<string>("--project", "-p");
+        var sourceOption = new Option<string>("--source", "-s");
+        var noteOption = new Option<string>("--note", "-n");
+        var linkOption = new Option<string>("--link", "-l");
+        var langOption = new Option<string>("--lang");
+        var starredOption = new Option<bool>("--star");
         
         command.Options.Add(tagsOption);
+        command.Options.Add(projectsOption);
+        command.Options.Add(sourceOption);
+        command.Options.Add(noteOption);
+        command.Options.Add(linkOption);
+        command.Options.Add(langOption);
+        command.Options.Add(starredOption);
         
         command.SetAction(result =>
         {
@@ -52,15 +64,31 @@ public class SaveCommand
             }
             
             var tags = result.GetValue(tagsOption) ?? [];
-            Execute(content, tags.ToList());
+            var project = result.GetValue(projectsOption);
+            var source = result.GetValue(sourceOption);
+            var note = result.GetValue(noteOption);
+            var link = result.GetValue(linkOption);
+            var language = result.GetValue(langOption);
+            var starred = result.GetValue(starredOption);
+            Execute(content, project, source, note, 
+                link, language, starred, tags.ToList());
         });
 
         return command;
     }
 
-    private void Execute(string message, List<string> tags)
+    private void Execute(string content, string? project, string? source,
+        string? note, string? link, string? language, bool starred, List<string> tags)
     {
-        var entry = new Entry(message, tags, DateTime.Now);
+        var entry = new Entry(content, tags, DateTime.Now)
+        {
+            Project = project,
+            Source = source,
+            Note = note,
+            Link = link,
+            Language = language,
+            IsStarred = starred
+        };
         
         _repository.Save(entry);
         _console.WriteLine("Entry saved successfully.");
--- src/DevBank/Models/Entry.cs
+++ src/DevBank/Models/Entry.cs
@@ -3,6 +3,13 @@
 public class Entry
 {
     public string Content { get; set; }
+    public string? Project { get; set; }
+    public string? Source { get; set; }
+    public string? Priority { get; set; }
+    public string? Note { get; set; }
+    public string? Language { get; set; }
+    public string? Link { get; set; }
+    public bool IsStarred { get; set; } = false;
     public List<string> Tags { get; set; } = [];
     public DateTime CreatedAt { get; set; }
 
